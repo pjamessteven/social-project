da85323c17d160d456663c29794374e0c912eb4b
Your Name <pjamessteven@gmail.com>
2025-09-21 11:27:12 +1200

    feat: update support page with detailed information on detransition support and therapy

--- app/support/page.tsx
+++ app/support/page.tsx
@@ -1,9 +1,12 @@
 export default async function StudiesPage() {
   return (
     <div className="prose prose-sm dark:prose-invert pb-16 lg:pt-8">
-      <h1>Get Support</h1>
-
+      <h1>Get Detransition Help & Support From Real People</h1>
       <h2>Online Support groups:</h2>
+      <p>
+        One of the best ways to get support is to talk to real people who have
+        been there themselves.
+      </p>
       <ul>
         <li>
           <a
@@ -36,15 +39,72 @@ export default async function StudiesPage() {
           </a>
         </li>
       </ul>
-      <h2>Get one-on-one therapy:</h2>
-      
-      <h3>The Detrans Foundation</h3>
+      <h2>Find a therapist who practices gender exploratory therapy:</h2>
       <p>
-        The Detrans Foundation provides resources and support for detransitioners, 
-        including access to qualified therapists who understand detransition experiences.
+        <b>What is gender exploratory therapy?</b>
       </p>
-      
-      <h4>Dr. Kirsty Entwistle</h4>
+      <p>
+        Gender exploratory therapy simply means that your therapist doesn't rush
+        to affirm your identity, and instead helps you explore why you might
+        feel this way. Gender activists hate this idea because it conflicts with
+        their belief that gender (a social construct) is something innate within
+        us, and the idea that people can be 'born in the wrong body'.
+      </p>
+      <p>
+        If you google gender exploratory therapy, you will find all sorts of
+        articles and studies by gender activists who compare it to gay
+        conversion therapy. For example the study{" "}
+        <a
+          href="https://pmc.ncbi.nlm.nih.gov/articles/PMC10018052/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="underline"
+        >
+          Interrogating Gender-Exploratory Therapy
+        </a>{" "}
+        was published in 2022 by Florence Ashley, a{" "}
+        <a
+          href="https://en.wikipedia.org/wiki/Florence_Ashley"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="underline"
+        >
+          trans woman
+        </a>
+        . Studies in this field by people who currently identify as trans should
+        be treated with skepticism, as they are motivated to protect the
+        validity of their identity.
+        <p>
+          The notion that gender exploratory therapy is evil and wrong is
+          predicated on the idea that gender is something innate within us, but
+          the mere existence of detrans people causes this theory to fall over
+          immediatly. Gender activists then fall back to saying that detrans
+          people were never 'real trans' people, but detrans experiences tell us
+          that they were fully 'in it', until they weren't any more.
+        </p>
+        <p>
+          It's also noteworthy that scientists are still yet to discover a 'gay
+          gene', and that many people who self-identified as gay or straight at
+          one point also come to the realisation that sexuality is not as black
+          and white as society once told them it is.
+        </p>
+      </p>
+      <a
+        href="https://www.detransfoundation.com/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="underline"
+      >
+        <h3>
+          <b>The Detrans Foundation</b>
+        </h3>
+      </a>
+      <p>
+        The Detrans Foundation provides resources and support for
+        detransitioners, including access to qualified therapists who understand
+        detransition experiences. The following therapists are part of the
+        Detrans foundation:
+      </p>
       <p>
         <a
           href="https://www.detransfoundation.com/dr-kirsty-entwistle.html"
@@ -54,12 +114,12 @@ export default async function StudiesPage() {
         >
           Dr. Kirsty Entwistle
         </a>{" "}
-        is a Clinical Psychologist who previously worked at the NHS gender identity development 
-        service for under 18s in Leeds. She is registered with the UK Health Care Professions 
-        Council (HCPC) and offers online consultations by secure videocall.
+        is a Clinical Psychologist who previously worked at the NHS gender
+        identity development service for under 18s in Leeds. She is registered
+        with the UK Health Care Professions Council (HCPC) and offers online
+        consultations by secure videocall.
       </p>
-      
-      <h4>Anastassis Spiliadis</h4>
+
       <p>
         <a
           href="https://www.detransfoundation.com/anastassis-spilliadis.html"
@@ -69,10 +129,30 @@ export default async function StudiesPage() {
         >
           Anastassis Spiliadis
         </a>{" "}
-        is a Systemic & Family Psychotherapist who worked for four years at the Gender Identity 
-        Development Service at the Tavistock, where he led the Family Therapy & Consultation Service. 
-        He developed the Gender Exploratory Model and has extensive experience working with 
-        gender-questioning individuals and detransitioners.
+        is a Systemic & Family Psychotherapist who worked for four years at the
+        Gender Identity Development Service at the Tavistock, where he led the
+        Family Therapy & Consultation Service. He resigned the Tavistock as he
+        disagreed with the lack of a therapeutic model in understanding gender
+        identity difficulties. He developed the Gender Exploratory Model and has
+        extensive experience working with gender-questioning individuals and
+        detransitioners.
+      </p>
+
+      <a
+        href="https://www.therapyfirst.org/find/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="underline"
+      >
+        {" "}
+        <h3>
+          <b>Therapy First</b>
+        </h3>
+      </a>
+      <p>
+        Therapy First is one of the only online directories for gender
+        exploratory therapists. Therapy first requires all pracitioners to agree
+        to the following membership statement:
       </p>
     </div>
   );
