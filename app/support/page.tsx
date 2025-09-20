41fd3fc1c82e71fa1f5bd7e3b245da8f90ea8cfc
Your Name <pjamessteven@gmail.com>
2025-09-21 10:38:33 +1200

    feat: create support page component

---
+++ app/support/page.tsx
@@ -0,0 +1,12 @@
+export default async function StudiesPage() {
+  return (
+    <div className="prose prose-sm dark:prose-invert pb-16 lg:pt-8">
+      <h1>Get Support</h1>
+
+      <h2>Online Support groups:</h2>
+      <ul>
+        <li>Detrans Wiki </li>
+      </ul>
+    </div>
+  );
+}
