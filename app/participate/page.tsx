7b3afbdbcb177b7a3e34338a362765e77f7a6de1
Your Name <you@example.com>
2025-11-02 11:59:19 +1300

    update manifesto

---
+++ app/participate/page.tsx
@@ -0,0 +1,10 @@
+export default async function ParticipatePage() {
+  return (
+    <div className="prose dark:prose-invert pb-16 lg:pt-8">
+      <h1 className="">Current Academic Studies</h1>
+      <p>
+         Detransition rates are increasing and 
+      </p>
+    </div>
+  );
+}
